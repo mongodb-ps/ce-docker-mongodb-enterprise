763c79427196b3dda02cd64de6cfe0c219a4785c
Yaoxing <yaoxing.zhang@gmail.com>
2024-01-17 11:30:38 +0100

    - change automation agent version

--- mongo/config.sh
+++ mongo/config.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 source ../config.sh
-export PROJECT_ID="64135d916d1e4f2360663de6" # mmsGroupId in Ops Manager
-export API_KEY="64135dae6d1e4f2360663e4c3e206c4356bd343aa3e6aeb14f8b7e93" # mmsApiKey in Ops Manager
+export PROJECT_ID="65a79e743c03e801e587d697" # mmsGroupId in Ops Manager
+export API_KEY="65a79f4a3c03e801e587d898c0e5cfac3086d6e0f852c94bdd2379aa" # mmsApiKey in Ops Manager
 export OM_URL="http://host.docker.internal:8080" # mmsBaseUrl in Ops Manager
-export AA_URL="$OM_URL/download/agent/automation/mongodb-mms-automation-agent-manager-12.0.18.7668-1.aarch64.amzn2.rpm"
+export AA_URL="$OM_URL/download/agent/automation/mongodb-mms-automation-agent-manager-12.0.28.7763-1.aarch64.amzn2.rpm"
 export INSTANCES=3
