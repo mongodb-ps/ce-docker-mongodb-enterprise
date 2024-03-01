33d56da95dcef518d0f65bd6ec2bc50c68a351ac
Yaoxing <yaoxing.zhang@gmail.com>
2024-03-02 00:35:12 +0100

    + move to amazon linux 2 because it supports older version of MongoDB

--- mongo/bootstrap.sh
+++ mongo/bootstrap.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 
 # Enterprise dependencies
-apt-get update; \
-apt-get install -y libcurl4 libldap-2.5-0 libwrap0 libsasl2-2 libsasl2-modules libsasl2-modules-gssapi-mit snmp openssl curl; \
+yum update -y; \
+yum install -y cyrus-sasl cyrus-sasl-gssapi cyrus-sasl-plain krb5-libs libcurl openldap openssl xz-libs; \
 echo "The following parameters are used to build mongo image: $OM_URL,$PROJECT_ID,$API_KEY"; \
 unset http_proxy; \
 unset https_proxy; \
 curl -OL $AA_URL; \
-# dkpg thinks aarch64 is different from arm64. This will fix the problem.
-dpkg --add-architecture aarch64; \
-dpkg -i *.deb; \
+rpm -ivh *.rpm; \
 sed -i "s%mmsGroupId=.*%mmsGroupId=$PROJECT_ID%" /etc/mongodb-mms/automation-agent.config; \
 sed -i "s%mmsApiKey=.*%mmsApiKey=$API_KEY%" /etc/mongodb-mms/automation-agent.config; \
 sed -i "s%mmsBaseUrl=.*%mmsBaseUrl=$OM_URL%" /etc/mongodb-mms/automation-agent.config; \
 mkdir -p /data/{db,log}; \
-chown mongodb:mongodb -R /data
-rm -rf /var/lib/apt/lists/* *.deb
+chown mongod:mongod -R /data; \
+yum clean all; \
+rm -rf /var/cache/yum; \
--- mongo/startup.sh
+++ mongo/startup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 mkdir -p /var/log/mongodb-mms-automation/
-chown mongodb:mongodb /var/log/mongodb-mms-automation/
-chown mongodb:mongodb /data/db /data/log
+chown mongod:mongod /var/log/mongodb-mms-automation/
+chown mongod:mongod /data/db /data/log
 /opt/mongodb-mms-automation/bin/mongodb-mms-automation-agent -f /etc/mongodb-mms/automation-agent.config
