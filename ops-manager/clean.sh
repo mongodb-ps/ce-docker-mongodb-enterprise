3a54cbb7302f773405ccd44422208007a5d0bd73
Yaoxing <yaoxing.zhang@gmail.com>
2026-01-28 22:37:01 +0100

    - remove files that we don't need.

--- ops-manager/clean.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-source config.sh
-docker image rm $DOCKER_USERNAME/mongo-enterprise:$MONGODB_VERSION
-docker image rm $DOCKER_USERNAME/ops-manager:$OM_VERSION
-docker volume prune
--- ops-manager/om/startup.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-chown mongodb-mms:mongodb-mms -R /mongodb-mms/mongodb-releases /mongodb-mms/logs
-/mongodb-mms/bin/mongodb-mms start
-tail -f /mongodb-mms/logs/mms0.log
