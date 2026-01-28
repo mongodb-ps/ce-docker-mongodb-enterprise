b06bb8bea7f9445dea6b2ee46027c715e86de4af
Yaoxing <yaoxing.zhang@gmail.com>
2026-01-28 22:37:48 +0100

    - rewrite the image building logic. - use an intuitive way to configure the image.

---
+++ ops-manager/om/setup.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+DEBIAN_FRONTEND=noninteractive 
+ROOT_USER=$1
+ROOT_PWD=$2
+# Add user/group for Ops Manager
+groupadd --gid 999 --system mongodb-mms;
+useradd --uid 999 --system --gid mongodb-mms --home-dir /mongodb-mms mongodb-mms;
+
+# Install dependencies
+apt update;
+apt install -y openssl net-tools fontconfig;
+
+# ADD instruction will automatically extract the tarball into /mongodb-mms
+# Adding this line to verify the content
+ls -l /mongodb-mms/
+
+# Get JDK version from package
+JAVA_VERSION=$(grep "JAVA_VERSION=" /mongodb-mms/jdk/release | cut -d'"' -f2);
+MAJOR_VERSION=$(echo $JAVA_VERSION | cut -d'.' -f1);
+JDK_PKG_NAME="openjdk-${MAJOR_VERSION}-jdk"
+
+# Install system JDK
+apt install -y $JDK_PKG_NAME;
+
+# Replace JDK with system JDK
+rm -rf /mongodb-mms/jdk;
+ln -s /usr/lib/jvm/java-$MAJOR_VERSION-openjdk-arm64 /mongodb-mms/jdk;
+chown mongodb-mms:mongodb-mms -R /mongodb-mms/;
+ls -l /mongodb-mms/jdk/
+
+# Edit Ops Manager config file
+cd /mongodb-mms/conf/;
+sed -i 's%mongo.mongoUri=.*%mongo.mongoUri=mongodb://'$ROOT_USER':'$ROOT_PWD'@appdb:27017/?maxPoolSize=150%' conf-mms.properties;
+sed -i 's%ENC_KEY_PATH=.*%ENC_KEY_PATH=/gen.key%' mms.conf;
+chown mongodb-mms:mongodb-mms /gen.key;
+chmod 400 /gen.key
+
+# Set entrypoint script permissions
+chmod +x /usr/local/bin/entrypoint.sh;
+
+# Clean up
+apt clean;
+rm -rf /var/lib/apt/lists/*
