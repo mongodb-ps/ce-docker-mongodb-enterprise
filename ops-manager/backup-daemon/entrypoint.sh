e8c847447170f64ce131fba9ec0bb6accddc2a64
Yaoxing <yaoxing.zhang@gmail.com>
2026-01-29 09:21:40 +0100

    - use exec for tailing log file.

--- ops-manager/backup-daemon/entrypoint.sh
+++ ops-manager/backup-daemon/entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 chown mongodb-mms:mongodb-mms -R /mongodb-mms/mongodb-releases /mongodb-mms/logs
 
-/mongodb-mms/bin/mongodb-mms start
-exec tail -f /mongodb-mms/logs/mms0.log
+/mongodb-mms/bin/mongodb-mms-backup-daemon start
+exec tail -f /mongodb-mms/logs/daemon.log
