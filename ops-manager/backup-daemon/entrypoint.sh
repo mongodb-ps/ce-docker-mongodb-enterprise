083034bb24d6cf0a4fa5a70734eaca4a1ea22c11
Yaoxing <yaoxing.zhang@gmail.com>
2026-01-29 14:12:31 +0100

    - ops manager container should not start backup daemon.

--- ops-manager/backup-daemon/entrypoint.sh
+++ ops-manager/backup-daemon/entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 chown mongodb-mms:mongodb-mms -R /mongodb-mms/mongodb-releases /mongodb-mms/logs
 
-/mongodb-mms/bin/mongodb-mms-backup-daemon start
+/mongodb-mms/bin/mongodb-mms start_backup_daemon
 exec tail -f /mongodb-mms/logs/daemon.log
--- ops-manager/om/entrypoint.sh
+++ ops-manager/om/entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 chown mongodb-mms:mongodb-mms -R /mongodb-mms/mongodb-releases /mongodb-mms/logs
 
-/mongodb-mms/bin/mongodb-mms start
+/mongodb-mms/bin/mongodb-mms start_mms
 exec tail -f /mongodb-mms/logs/mms0.log
