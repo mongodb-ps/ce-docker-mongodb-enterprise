a82b2c4732869d907aa2529de629552475ba318b
Yaoxing <yaoxing.zhang@gmail.com>
2026-01-30 17:50:45 +0100

    - keep restarting backup daemon until it is started successfully.

--- ops-manager/backup-daemon/entrypoint.sh
+++ ops-manager/backup-daemon/entrypoint.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 chown mongodb-mms:mongodb-mms -R /mongodb-mms/mongodb-releases /mongodb-mms/logs
+STATUS_CMD="/mongodb-mms/bin/mongodb-mms-backup-daemon status"
+START_CMD="/mongodb-mms/bin/mongodb-mms-backup-daemon start"
 
-/mongodb-mms/bin/mongodb-mms start_backup_daemon
+# Start service until successfully started
+until $STATUS_CMD | grep -q 'OK'; do
+    $START_CMD
+    echo "Restart backup daemon in 5 seconds..."
+    sleep 5
+done
 exec tail -f /mongodb-mms/logs/daemon.log
